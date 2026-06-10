56f88b730cc62a11aeeb076feaac88a7d3067f2a
henriquesantos2004 <henrique21m.santos@gmail.com>
2026-06-10 15:25:32 +0100

    fotos para cada capa de filme e serie

--- app/src/main/java/com/example/projetofinalprogramacao/FonteDeDados.kt
+++ app/src/main/java/com/example/projetofinalprogramacao/FonteDeDados.kt
@@ -15,13 +15,13 @@ data class CategoriaGenero(
 object FonteDeDados {
     // Filmes de Comédia
     val filmesComedia = listOf(
-        ItemMedia(1, "Shrek", "Um ogre verde vê a sua solidão invadida por personagens de contos de fadas.", android.R.drawable.ic_menu_gallery),
-        ItemMedia(2, "A Máscara", "Um funcionário bancário tímido descobre uma máscara mágica que o transforma num herói louco.", android.R.drawable.ic_menu_gallery)
+        ItemMedia(1, "Shrek", "Um ogre verde vê a sua solidão invadida por personagens de contos de fadas.", R.drawable.shrek),
+        ItemMedia(2, "A Máscara", "Um funcionário bancário tímido descobre uma máscara mágica que o transforma num herói louco.", R.drawable.themask)
     )
 
     // Filmes de Terror
     val filmesTerror = listOf(
-        ItemMedia(3, "O Telefone Negro", "Um rapaz de 13 anos é raptado e começa a receber chamadas de um telefone desligado.", android.R.drawable.ic_menu_gallery)
+        ItemMedia(3, "O Telefone Negro", "Um rapaz de 13 anos é raptado e começa a receber chamadas de um telefone desligado.", R.drawable.otelefonenegroo)
     )
 
     val generosFilmes = listOf(
@@ -31,7 +31,7 @@ object FonteDeDados {
 
     // Séries de Comédia
     val seriesComedia = listOf(
-        ItemMedia(4, "The Office", "O quotidiano cómico e absurdo dos funcionários de uma empresa de papel.", android.R.drawable.ic_menu_gallery)
+        ItemMedia(4, "The Office", "O quotidiano cómico e absurdo dos funcionários de uma empresa de papel.", R.drawable.theofficee)
     )
 
     val generosSeries = listOf(
